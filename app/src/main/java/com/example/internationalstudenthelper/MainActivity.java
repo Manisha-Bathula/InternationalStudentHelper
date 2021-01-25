c2f59af27a7ac4767e589eabc68fda94f78dc8a2
Vnktprdh <venkatpardha@ymail.com>
2021-01-24 23:28:02 -0500

    Completed splashscreen,Login screen,google login,normal auntentication.

--- app/src/main/java/com/example/internationalstudenthelper/MainActivity.java
+++ app/src/main/java/com/example/internationalstudenthelper/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.internationalstudenthelper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },10000);
+        }, 2000);
 
     }
 }
