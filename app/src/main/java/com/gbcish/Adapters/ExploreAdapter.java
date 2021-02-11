941206686e5ab0133ac9f05e73fe217453039d86
vnktprdh <venkatpardha@ymail.com>
2021-02-11 10:19:01 -0500

    ExploreActivity.java , displaying of all posted data in recycler view is done

---
+++ app/src/main/java/com/gbcish/Adapters/ExploreAdapter.java
@@ -0,0 +1,127 @@
+package com.gbcish.Adapters;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
+import com.example.internationalstudenthelper.R;
+import com.gbcish.CommonFunctions.Utils;
+import com.gbcish.models.PostModel;
+
+import java.util.List;
+
+public class ExploreAdapter extends RecyclerView.Adapter<ExploreAdapter.MyViewHolder> {
+
+    private List<PostModel> postModelList;
+    private Context context;
+    private OnItemClickListener onItemClickListener;
+
+    public ExploreAdapter(List<PostModel> postModels, Context context) {
+        this.postModelList = postModels;
+        this.context = context;
+    }
+
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.explore_list_items, parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
+        final PostModel model = postModelList.get(position);
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.placeholder(Utils.getRandomDrawbleColor());
+        requestOptions.error(Utils.getRandomDrawbleColor());
+        requestOptions.diskCacheStrategy(DiskCacheStrategy.ALL);
+        requestOptions.centerCrop();
+//
+//        Glide.with(context)
+//                .load(Uri.parse(model.getImageUrl().get(0).imageUrl))
+//                .into(holder.imageView);
+
+        Glide.with(context)
+                .load(model.getImageUrl().get(0).imageUrl)
+                .apply(requestOptions)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+//                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+//                        holder.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                })
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holder.imageView);
+
+
+
+        holder.title.setText(model.getPost_title());
+        holder.price.setText(model.getPost_price());
+        holder.location.setText(model.getPost_location());
+        holder.category.setText(model.getPost_category());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postModelList.size();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position, PostModel postModel);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, price, category, location;
+        ImageView imageView;
+        ProgressBar progressBar;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.tv_title);
+            location = itemView.findViewById(R.id.tv_location);
+            price = itemView.findViewById(R.id.tv_price);
+            category = itemView.findViewById(R.id.tv_category);
+            imageView = itemView.findViewById(R.id.imageView);
+
+//            progressBar = itemView.findViewById(R.id.prograss_load_photo);
+
+
+        }
+    }
+}
--- app/src/main/java/com/gbcish/ExploreActivity.java
+++ app/src/main/java/com/gbcish/ExploreActivity.java
@@ -1,16 +1,74 @@
 package com.gbcish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.internationalstudenthelper.R;
+import com.gbcish.Adapters.ExploreAdapter;
+import com.gbcish.models.PostModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class ExploreActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class ExploreActivity extends AppCompatActivity implements ExploreAdapter.OnItemClickListener {
+    private DatabaseReference databaseRef;
+    private ArrayList<PostModel> postModelList=new ArrayList<>();
+    private RecyclerView rentalRecyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private ExploreAdapter exploreAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore);
+
+        rentalRecyclerView=findViewById(R.id.rentalRecyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        rentalRecyclerView.setLayoutManager(layoutManager);
+        rentalRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        rentalRecyclerView.setNestedScrollingEnabled(false);
+
+        exploreAdapter = new ExploreAdapter(postModelList, this);
+        exploreAdapter.setOnItemClickListener(ExploreActivity.this);
+        rentalRecyclerView.setAdapter(exploreAdapter);
+
+
+        databaseRef = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference addRentalPosts = databaseRef.child("AddRentalPosts");
+
+        addRentalPosts.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snap: snapshot.getChildren()){
+                    PostModel c = snap.getValue(PostModel.class);
+                    Log.d("Categories: ", c.getPost_location() + " " + c.getPost_description());
+                    postModelList.add(c);
+                    exploreAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemClick(View view, int position, PostModel postModel) {
+
     }
 }
