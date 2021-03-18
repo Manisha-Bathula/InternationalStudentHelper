e0e1d66eeccd986584adf7c725596d4100e987b8
Manisha Bathula <manishabathula9@gmail.com>
2021-03-17 22:43:25 -0400

    Created aboutus page. AboutUsActivity.java and xml. showing Aboutuspage in a webview

--- app/src/main/java/com/gbcish/AboutUsActivity.java
+++ app/src/main/java/com/gbcish/AboutUsActivity.java
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.internationalstudenthelper.R;
 
 public class AboutUsActivity extends AppCompatActivity {
     ActionBar actionBar;
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +25,14 @@ public class AboutUsActivity extends AppCompatActivity {
         //Action Bar
         getSupportActionBar().setTitle("About Us");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        webView = (WebView) findViewById(R.id.webview_aboutus);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
-    public void gotodashboard(View view) {
-        Intent intent = new Intent(AboutUsActivity.this, DashboardActivity.class);
-        startActivity(intent);
-    }
 
-    public void gotopost(View view) {
-        Intent intent1 = new Intent(AboutUsActivity.this, PostActivity.class);
-        startActivity(intent1);
-    }
-
-    public void gotocurrency(View view) {
-        Intent intent2 = new Intent(AboutUsActivity.this, CurrencyConvertorActivity.class);
-        startActivity(intent2);
-    }
-
-    public void gotocontact(View view) {
-        Intent intent4 = new Intent(AboutUsActivity.this, ContactUsActivity.class);
-        startActivity(intent4);
-    }
 }
